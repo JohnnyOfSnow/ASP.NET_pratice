da058a726163e6472c58bf98805a793aaa08d036
johnnyOfSnow <good258johnny@yahoo.com.tw>
2020-03-26 12:51:21 +0900

    Add sideProject3 for uploadImage test

---
+++ sideProject_ImgSystem/shoppingWeb/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+using shoppingWeb.Models;
+
+namespace shoppingWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+
+        public ActionResult Product()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Product(FormCollection post)
+        {
+            string fname = post["inputFileName"];
+            string froute = post["inputFileRoute"];
+            int price = Convert.ToInt32(post["price"]);
+            MyImage img = new MyImage(fname, froute, price);
+            return View(img);      
+        }
+    }
+}
---
+++ sideProject_ImgSystem/shoppingWeb/Models/MyImage.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace shoppingWeb.Models
+{
+    public class MyImage
+    {
+        public string fileName { get; set; }
+        public string fileRoute { get; set; }
+        public int price { get; set; }
+
+        public MyImage()
+        {
+            fileName = "";
+            fileRoute = "";
+            price = 0;
+        }
+
+        public MyImage(string _fileName, string _fileRoute, int _price)
+        {
+            fileName = _fileName;
+            fileRoute = _fileRoute;
+            price = _price;
+        }
+
+
+    }
+}
